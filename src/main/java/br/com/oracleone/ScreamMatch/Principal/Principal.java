30cb8737aad473f43eed55a12e0c477925f99b0b
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-05-24 23:41:56 -0300

    feat: finished module 3

--- src/main/java/br/com/oracleone/ScreamMatch/Principal/Principal.java
+++ src/main/java/br/com/oracleone/ScreamMatch/Principal/Principal.java
@@ -3,12 +3,15 @@ package br.com.oracleone.ScreamMatch.Principal;
 import br.com.oracleone.ScreamMatch.model.DadosEpisodio;
 import br.com.oracleone.ScreamMatch.model.DadosSerie;
 import br.com.oracleone.ScreamMatch.model.DadosTemporada;
+import br.com.oracleone.ScreamMatch.model.Episodio;
 import br.com.oracleone.ScreamMatch.service.ConsumoApi;
 import br.com.oracleone.ScreamMatch.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.Locale;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
     private final Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
@@ -52,5 +55,56 @@ public class Principal {
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 
         temporadas.forEach(System.out::println);
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t ->t.episodios().stream())
+                .toList();
+
+        System.out.println("Top 5 eps");
+        dadosEpisodios.stream()
+                .filter(e->!"N/A".equals(e.avaliacao()))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t ->t.episodios().stream()
+                .map(d-> new Episodio(t.numeroTemp(),d)))
+                .collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("A partir de qual data deseja mostrar os eps: ");
+        var ano = scanner.nextInt();
+        scanner.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano,1,1);
+
+        DateTimeFormatter dataFormatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e-> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: "+ e.getTemporada()+
+                        " Titulo:"+ e.getTitulo()+
+                        " Episodio: "+ e.getNumeroEp()+
+                        " Avaliação: "+ e.getAvaliacao()+
+                        " Data de Lançamento: " + e.getDataLancamento().format(dataFormatador)
+                        )
+                );
+
+
+        //toList = lista imutavel
+        //.collect(Collectors.toList()); = lista mutavel
+
+//        List<String> nomes = Arrays.asList("Paulo", "Ana", "Tentoi", "Bonout", "Zuloi");
+//
+//        nomes.stream()
+//                .sorted()
+//                .limit(3)
+//                .filter(n -> n.startsWith("P"))
+//                .map(n -> n.toUpperCase())
+//                .forEach(System.out::println);
     }
 }
